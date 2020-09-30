0b1297454e54609700b61a97849020d30b4d9cd4
Shajal Kumer <shajalkumer@gmail.com>
2020-09-30 23:48:16 +0600

    Updates

---
+++ precache-manifest.deba3189bdeaffc678bb8a85577c53db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b603e3901a0b06dc8a4f4fdc161e7c6",
+    "url": "/weather-app-react-pwa/index.html"
+  },
+  {
+    "revision": "0a139db2ca34f1489651",
+    "url": "/weather-app-react-pwa/static/css/main.7a487347.chunk.css"
+  },
+  {
+    "revision": "2fd0c47b5d8e6bcdcb95",
+    "url": "/weather-app-react-pwa/static/js/2.791f8b78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app-react-pwa/static/js/2.791f8b78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a139db2ca34f1489651",
+    "url": "/weather-app-react-pwa/static/js/main.64dbd132.chunk.js"
+  },
+  {
+    "revision": "2794ebda1f06ff507790",
+    "url": "/weather-app-react-pwa/static/js/runtime-main.acc8b847.js"
+  }
+]);
